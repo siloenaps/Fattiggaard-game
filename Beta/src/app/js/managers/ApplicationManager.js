2fb1415d1b3c32440411b2ec4ff57bf5c5fbc06b
siloenaps <str@siloen.dk>
2015-11-21 17:35:57 +0100

    Working iPad version Optimisation is work in progress

--- Beta/src/app/js/managers/ApplicationManager.js
+++ Beta/src/app/js/managers/ApplicationManager.js
@@ -31,8 +31,8 @@ var ApplicationManager = {
 
 
 		// Go to start
-		// FlowManager.gotoPage('0.0');
-		FlowManager.gotoPage('1.0.1');
+		FlowManager.gotoPage('0.0');
+		// FlowManager.gotoPage('1.0.1');
 		// FlowManager.gotoPage('2.5');
 
 		//console.log('Ticker.framerate:', Ticker.framerate);
